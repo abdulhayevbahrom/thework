4097e4a770ebbf6bf2b85b27cd38e2118e1df2e1
Flowernoza <cadillac2111@Salmoorbeks-MacBook-Pro.local>
2023-09-09 16:35:51 +0500

    changed

--- src/components/infoCard/InfoCard.jsx
+++ src/components/infoCard/InfoCard.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import './Infocard.css'
 
 function InfoCard() {
     return (
-        <div>
-            how to get a job
+      <div className="InfoCard">
+        <div className="InfoCardBox">
+          <p>Pre-opening recruitment</p>
+          <span>
+            Turn-key recruitment for projects of any scale. Permanent and short
+            term. Trust this to us and concentrate on a thousand other things.
+          </span>
         </div>
-    )
+        <div className="InfoCardBox">
+          <p>Walk-in interviews</p>
+          <span>
+            Turn-key recruitment for projects of any scale. Permanent and short
+            term. Trust this to us and concentrate on a thousand other things.
+          </span>
+        </div>
+        <div className="InfoCardBox">
+          <p>Ongoing recruitment</p>
+          <span>
+            Providing new talents for your business when required. Get a quick
+            replacement or grow your team with reliable partner who knows your
+            preferences.
+          </span>
+        </div>
+      </div>
+    );
 }
 
 export default InfoCard
--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import Banner from '../../components/banner/Banner'
-import Header from "../../components/header/Header"
-import About from '../../components/about/About'
-import InfoCard from '../../components/infoCard/InfoCard'
-import Section from '../../components/section/Section'
+import React from "react";
+import Banner from "../../components/banner/Banner";
+import Header from "../../components/header/Header";
+import About from "../../components/about/About";
+import InfoCard from "../../components/infoCard/InfoCard";
+import Section from "../../components/section/Section";
+// import Conditions from "../../components/conditions/Conditions";
 
 function Home() {
-    return (
-        <div className='home'>
-            <Header />
-            <Banner />
-            <About />
-            <InfoCard />
-            <Section />
-            {/* <Carousel/> */}
-        </div>
-    )
+  return (
+    <div className="home">
+      <Header />
+      <Banner />
+      <About />
+      {/* <Conditions /> */}
+      <InfoCard />
+      <Section />
+      {/* <Carousel/> */}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
