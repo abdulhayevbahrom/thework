8c5c820f2207ad78e636d0747f405b523286b000
avazbek2001 <ssamuray225@gmail.com>
2023-09-09 13:33:14 +0500

    i have created info cards and section with media

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import './Section.css'
+import image from './startup-593341_1920.jpg'
 function Section() {
     return (
-        <div>
-            BEST HOSPITALITY BRANDS ARE LOOKING FOR NEW TALENTS WITH US. FIND SUITABLE POSITIONS AND APPLY RIGHT NO
-        </div>
+        <div className='hospitality'>
+        <h1>BEST HOSPITALITY BRANDS ARE LOOKING FOR NEW TALENTS WITH US. FIND SUITABLE POSITIONS AND APPLY RIGHT NOW!</h1>
+        <button><a href="/">Go to list job</a></button>
+      
+    </div>
     )
 }
 
