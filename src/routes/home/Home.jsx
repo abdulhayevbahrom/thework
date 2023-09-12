cdcfe4aff4d2801ccf3f8430e5732811cb7c3cb1
abdulloh <adxamovabdulloh06@gmail.com>
2023-09-12 15:14:49 +0500

    carousel changed abdulloh

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -5,10 +5,12 @@ import About from '../../components/about/About'
 import InfoCard from '../../components/infoCard/InfoCard'
 import Section from '../../components/section/Section'
 import HomeCarousel from '../../components/homeCarousel/HomeCarousel'
+import Crousel11 from '../../components/crousel11/Carousel11'
 
 function Home() {
     return (
         <div className='home'>
+            <Crousel11 />
             <HomeCarousel />
             <Header />
             <Banner />
