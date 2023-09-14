70a98a08ab224c30d2d649b70757ed4f274fe058
abdulloh <adxamovabdulloh06@gmail.com>
2023-09-14 15:17:58 +0500

    hello

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,29 +1,161 @@
 import React from 'react'
 import './Sidebar.css'
-import { FaGlobe } from 'react-icons/fa'
+import { BiUserPlus, BiSearch } from 'react-icons/bi'
+import { AiOutlineBars } from 'react-icons/ai'
+import {FiLogIn} from 'react-icons/fi'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             <h1>Logo</h1>
+            <AiOutlineBars className='sidebar_bars' />
+              <div className='sidebar_serch'>
+                    <BiSearch className='sidebar_serch_icon'/>
+                    <input type="text" placeholder='Search' />
+                </div>
             <ul>
+              
                 <li>
-                    <FaGlobe className='svg' />
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
                     <span>All Countries</span>
                 </li>
                 <li>
-                    <FaGlobe className='svg' />
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
                     <span>UAE</span>
                 </li>
                 <li>
-                    <FaGlobe className='svg' />
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
                     <span>Saudi Arabiya</span>
                 </li>
                 <li>
-                    <FaGlobe className='svg' />
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
                     <span>Bahrain</span>
                 </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>All Countries</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>UAE</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Saudi Arabiya</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Saudi Arabiya</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Bahrain</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>All Countries</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>UAE</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Saudi Arabiya</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Bahrain</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>All Countries</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Saudi Arabiya</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Saudi Arabiya</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Bahrain</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>All Countries</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>UAE</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Saudi Arabiya</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Bahrain</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>All Countries</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Saudi Arabiya</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Saudi Arabiya</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>Bahrain</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>All Countries</span>
+                </li>
+                <li>
+                    <BiUserPlus className='svg' />
+                    <div className='sidebar_center_line'></div>
+                    <span>UAE</span>
+                </li>
+
             </ul>
+            <div className="sidebar_login_out">
+                <h5>Login out</h5>
+                <FiLogIn style={{color:"red"}} className='MdLogin'/>
+            </div>
         </div>
     )
 }
