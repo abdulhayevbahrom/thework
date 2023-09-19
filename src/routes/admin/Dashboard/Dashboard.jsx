56e7c653971884df214b1799b3033756ecf77ae0
Flowernoza <cadillac2111@Salmoorbeks-MacBook-Pro.local>
2023-09-19 10:51:50 +0500

    smth

--- src/routes/admin/Dashboard/Dashboard.jsx
+++ src/routes/admin/Dashboard/Dashboard.jsx
@@ -1,9 +1,4 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import AllAdmins from "../AllAdmins/AllAdmins";
-import CreateJob from "../createJob/CreateJob";
-import AdminLogin from "../createAdmin/AdminLogin";
-import Vacancy from "../alljobs/VacancyCard";
 import "./Dashboard.css";
 function Dashboard() {
   let allJobs = localStorage.getItem("alljobs")?.length;
@@ -13,29 +8,37 @@ function Dashboard() {
       link: "/admin/create-job",
       p: "Create",
       marked: "job",
-      color: "crimson",
       span: "Create some jobs here! Let's add interesting and modern professions ",
+      bColor: "blue",
+      color: "crimson",
+      borderRadius: "45px 10px 10px 10px",
     },
     {
       link: "/admin/create-admin",
       p: "ADD",
-      color: "orange",
       marked: "Admin",
       span: "You can add new admins that can manage job information",
+      color: "orange",
+      bColor: "green",
+      borderRadius: "10px 45px 10px 10px",
     },
     {
       link: "/admin/alljobs",
       p: allJobs,
-      color: "green",
       marked: "jobs",
       span: "We have currently. Find your own job with us !",
+      color: "green",
+      bColor: "gold",
+      borderRadius: "10px 10px 10px 45px",
     },
     {
       link: "/admin/all-admins",
       p: AllAdmins,
-      color: "dodgerblue",
       marked: "Admins",
       span: "These admins are monitoring 'TheWork' site.",
+      color: "dodgerblue",
+      bColor: "brown",
+      borderRadius: "10px 10px 45px 10px",
     },
   ];
   return (
@@ -43,14 +46,14 @@ function Dashboard() {
       <div className="overlay"></div>
       {data.map((item, index) => (
         <Link
-          style={{ backgroundColor: item.color }}
+          style={{ backgroundColor: item.color}}
           to={item.link}
           key={index}
           className="boards"
         >
           <div className="paragraph">
             <p>{item.p}</p>
-            <b >/{item.marked}</b>
+            <b style={{ color: item.bColor }}>/{item.marked}</b>
           </div>
           <span>{item.span}</span>
         </Link>
