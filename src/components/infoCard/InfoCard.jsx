787c2b62d99c11c0662e4b894afab1e7778e146c
avazbek2001 <ssamuray225@gmail.com>
2023-09-19 17:44:12 +0500

    info changed

--- src/components/infoCard/InfoCard.jsx
+++ src/components/infoCard/InfoCard.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Infocard.css'
+import { FaSearch } from 'react-icons/fa';
+import { FaDesktop } from 'react-icons/fa';
+import { MdLaptop } from 'react-icons/md';
+import { FaClipboardCheck } from 'react-icons/fa';
 
 import findvacancy from './image_2023-09-07_11-15-47.png'
 import interview from './image_2023-09-07_11-15-57.png'
@@ -13,22 +17,22 @@ function InfoCard() {
       </h1>
       <div className="fullgetjobcards">
         <div className="getjob_cards">
-          <img src={findvacancy} alt="" />
+          <FaSearch/>
           <b>1. Finding a vacancy</b>
-          <p>Check available opportunities and news then you cangit apply <a href="/">here</a></p>
+          <p>Check available opportunities and news then you can apply or travel <a href="/">here</a></p>
         </div>
         <div className="getjob_cards">
-          <img src={interview} alt="" />
+          <FaDesktop/>
           <b>2. Interview with us</b>
           <p>You should complete a pre-screening interview with our management. </p>
         </div>
         <div className="getjob_cards">
-          <img src={employer} alt="" />
+          <MdLaptop/>
           <b>3. Interview with employer</b>
           <p>Impress your future employer during an online meeting and get a Job Offer</p>
         </div>
         <div className="getjob_cards">
-          <img src={newjob} alt="" />
+          <FaClipboardCheck/>
           <b>4. Starting new job!</b>
           <p>Prepare necessary documents, receive a visa and get ready to travel</p>
         </div>
